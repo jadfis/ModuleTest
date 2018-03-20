de83448f132f48379ae61577435963a337954603
jadfis <kurzojul11@hotmail.fr>
2018-03-20 23:48:19 +0100

    Init

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = function(sails) {
     var setup = require('./lib/setup.js');
    // var init = require('./lib/init.js');
     
-    gladys.on('ready', function(){
-        init();
-    });
+   // gladys.on('ready', function(){
+    //    init();
+   // });
 
     return {
       setup
